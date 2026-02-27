c144676af2ffb281e94540f569a4ac633f103a40
Jan <jmjuhalu@tlu.ee>
2026-02-27 11:06:48 +0200

    proovikontrolltöö

---
+++ 04_27.02-proovikontrolltoo1/filmrent/src/main/java/ee/jmjuhalu/filmrent/FilmrentApplication.java
@@ -0,0 +1,13 @@
+package ee.jmjuhalu.filmrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmrentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmrentApplication.class, args);
+	}
+
+}
---
+++ 04_27.02-proovikontrolltoo1/filmrent/src/test/java/ee/jmjuhalu/filmrent/FilmrentApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.jmjuhalu.filmrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmrentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
