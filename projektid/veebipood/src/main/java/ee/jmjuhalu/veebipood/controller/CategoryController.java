c69d01c12f14a2aa166195ee45164c317581fe78
Jan <jmjuhalu@tlu.ee>
2026-04-17 10:14:11 +0300

    deploy

--- projektid/veebipood/src/main/java/ee/jmjuhalu/veebipood/controller/CategoryController.java
+++ projektid/veebipood/src/main/java/ee/jmjuhalu/veebipood/controller/CategoryController.java
@@ -1,38 +1,38 @@
 package ee.jmjuhalu.veebipood.controller;
 
 import ee.jmjuhalu.veebipood.entity.Category;
+import ee.jmjuhalu.veebipood.entity.Product;
+import ee.jmjuhalu.veebipood.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
-
-//localhost: 8080
-//application.properties server.port=8090
-
 public class CategoryController {
+
     @Autowired
-
-    private ee.jmjuhalu.veebipood.repository.CategoryRepository categoryRepository;
-
+    private CategoryRepository categoryRepository;
 
     @GetMapping("categories")
     public List<Category> getCategories(){
-
         return categoryRepository.findAll();
     }
 
+    // TODO: categoies/admin --> kuvatakse koik kategooriad
+    // categories --> kuvatakse vaid kategooriad, kellel on moni toode
+
     @DeleteMapping("categories/{id}")
     public List<Category> deleteCategory(@PathVariable Long id){
-        categoryRepository.deleteById(id); //kustutan
-        return categoryRepository.findAll(); //uuenenud seis
+        categoryRepository.deleteById(id); // kustutan
+        return categoryRepository.findAll(); // uuenenud seis
     }
 
     @PostMapping("categories")
-    public List<Category> addcategory(@RequestBody Category category){
-        categoryRepository.save(category); //siin salvesatab
-        return categoryRepository.findAll(); //siin on uuenenud seis
+    public List<Category> addCategory(@RequestBody Category category){
+        categoryRepository.save(category); // siin salvestab
+        return categoryRepository.findAll(); // siin on uuenenud seis
     }
 
 }
--- projektid/veebipood/src/main/java/ee/jmjuhalu/veebipood/repository/ProductRepository.java
+++ projektid/veebipood/src/main/java/ee/jmjuhalu/veebipood/repository/ProductRepository.java
@@ -1,13 +1,22 @@
 package ee.jmjuhalu.veebipood.repository;
 
 import ee.jmjuhalu.veebipood.entity.Product;
+import org.springframework.data.domain.Page; //!!!!!!
+import org.springframework.data.domain.Pageable; //!!!!!!
 import org.springframework.data.jpa.repository.JpaRepository;
 
-// repository --> andmebaasiga suhtlemiseks, tema sees on küik funktsioonid, mida on võimalik andmebaasiga teha
+import java.util.List;
 
+//  repository --> andmebaasiga suhtlemiseks, tema sees on kõige funktsioonid, mida on võimalik andmebaasiga teha.
 
-public interface ProductRepository extends
-        JpaRepository<Product, Long> {
-
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    // igas Repository-s on voimalik seda klassi (tabeli kirjet) k2tte saada
+    // listinda voi yksikuna
+    // siin on voimalik:
+    // list<Product>
+    // Product
+    // int
+    // boolean
+    // void
+    Page<Product> findAllByCategoryId(Pageable pageable, Long categoryId);
 }
-
